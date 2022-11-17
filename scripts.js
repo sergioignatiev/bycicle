522e5f7491f71709fa1e35ae0a96e17a07973730
sergioIgnatev <sergioignatev17@gmail.com>
2022-11-17 22:58:34 +0300

    jq

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
+function Toggle(id,target,target2){
+    $(document).ready(function(){
+        $(id).click(function(){
+          $(target).slideToggle(1000);
+          $(target2).addClass("animated bounce");
+         
+        });
+      });
+}
+Toggle("#mountSwitch","#mountain",".imgmountain")
+Toggle("#womenSwitch","#women","#imgwomen")
+
 function mSW(){
     
     let mount=document.getElementById("mountain")
     if(mount.style.display=="none"){
-        mount.style.display="block";
+        
         document.querySelector('i').classList.remove("fa-angle-down")
         document.querySelector('i').classList.add("fa-angle-up")
     }else{
-        mount.style.display="none";
+        
         document.querySelector('i').classList.add("fa-angle-down")
         document.querySelector('i').classList.remove("fa-angle-up")
     }
@@ -16,12 +28,12 @@ function wSW(){
     
     let mount=document.getElementById("women")
     if(mount.style.display=="block"){
-        mount.style.display="none";
+        
         document.getElementsByClassName("fa")[1].classList.add("fa-angle-down")
         document.getElementsByClassName("fa")[1].classList.remove("fa-angle-up")
         
     }else{
-        mount.style.display="block";
+        
         document.getElementsByClassName("fa")[1].classList.add("fa-angle-up")
         document.getElementsByClassName("fa")[1].classList.remove("fa-angle-down");
        
